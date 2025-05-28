8abf2a80a4545e45307842b3db4ee38a888aa784
Soso42 <sosojanashvili@gmail.com>
2025-05-29 01:15:54 +0400

    added services

---
+++ src/main/java/org/example/service/impl/WorkSpaceServiceImpl.java
@@ -0,0 +1,54 @@
+package org.example.service.impl;
+
+import org.example.entity.WorkSpace;
+import org.example.exceptions.WorkSpaceNotFoundException;
+import org.example.repository.WorkSpaceRepository;
+import org.example.repository.impl.InMemoryWorkSpaceRepository;
+import org.example.service.WorkSpaceService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class WorkSpaceServiceImpl implements WorkSpaceService {
+
+    private static WorkSpaceServiceImpl currentInstance;
+
+    private final WorkSpaceRepository repository = new InMemoryWorkSpaceRepository();
+
+
+    private WorkSpaceServiceImpl() {}
+
+
+    @Override
+    public void save(WorkSpace workSpace) {
+        Long id = repository.findAll().getLast().getId();
+        workSpace.setId(++id);
+        repository.save(workSpace);
+    }
+
+    @Override
+    public void removeWorkSpace(Long id) {
+        if (repository.findById(id).isEmpty()) {
+            throw new WorkSpaceNotFoundException("Work Space Not Found with id: " + id);
+        }
+        repository.deleteById(id);
+    }
+
+    @Override
+    public List<WorkSpace> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<WorkSpace> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public static WorkSpaceServiceImpl getInstance() {
+        if (currentInstance == null) {
+            currentInstance = new WorkSpaceServiceImpl();
+        }
+        return currentInstance;
+    }
+
+}
