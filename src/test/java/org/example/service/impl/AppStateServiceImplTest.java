fa9e59a93ae48ecd697f968d00ff4508fd0cd45d
Soso42 <sosojanashvili@gmail.com>
2025-06-16 23:28:10 +0400

    added appStateService tests

---
+++ src/test/java/org/example/service/impl/AppStateServiceImplTest.java
@@ -0,0 +1,70 @@
+package org.example.service.impl;
+
+import org.example.repository.impl.InMemoryBookingRepository;
+import org.example.repository.impl.InMemoryWorkSpaceRepository;
+import org.example.service.AppStateService;
+import org.example.service.PersistenceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AppStateServiceImplTest {
+
+    @Mock
+    private InMemoryBookingRepository bookingRepository;
+    @Mock
+    private InMemoryWorkSpaceRepository workSpaceRepository;
+    @Mock
+    private PersistenceService persistenceService;
+    @InjectMocks
+    private AppStateServiceImpl appStateService;
+
+
+    @Test
+    void saveAllData() {
+        // Given
+        // When
+        appStateService.saveAllData();
+
+        // Then
+        verify(persistenceService, times(2)).saveToFile(any(), anyString());
+    }
+
+
+    @Test
+    void restoreAllData_whenFetchSuccess_thenUpdateRepositories() {
+        // Given
+        when(persistenceService.readFromFile(anyString())).thenReturn(new HashMap<>());
+
+        // When
+        appStateService.restoreAllData();
+
+        // Then
+        verify(bookingRepository, times(1)).setBookings(any());
+        verify(workSpaceRepository, times(1)).setWorkspaces(any());
+    }
+
+    @Test
+    void restoreAllData_whenCantReadFiles_thenDontUpdateRepositories() {
+        // Given
+        when(persistenceService.readFromFile(anyString())).thenReturn(null);
+
+        // When
+        appStateService.restoreAllData();
+
+        // Then
+        verify(bookingRepository, times(0)).setBookings(any());
+        verify(workSpaceRepository, times(0)).setWorkspaces(any());
+    }
+
+}
